a72184262a54a8ee242162ae7d877764b92cf8c6
Babar Mehmood <babarzaidi1122@gmail.com>
2024-11-19 14:59:24 +0500

    commit

--- figma/src/app/components/Hero.tsx
+++ figma/src/app/components/Hero.tsx
@@ -1,24 +1,44 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import Girl from '/public/Images/Image.png';
-import React from 'react'
+import Banner from '/public/Images/2.png';
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='flex h-[80%]'>
-        
-      <div className='w-1/2 flex flex-col justify-center items-start space-y-4 pl-[176px]'>
-        <h1 className='max-w-[496px] font-bold text-[40px]'>Learn new skills online with ease</h1>
-        <p className='max-w-[902px] text-[30px]'>Discover a wide range of courses covering a variety of subjects, taught by expert instructors.</p>
+    <div className='flex flex-col h-auto'>
+      {/* Hero Section */}
+      <div className='flex w-full h-[80%]'>
+        {/* Left Section */}
+        <div className='w-1/2 flex flex-col justify-center items-start space-y-4 pl-[176px]'>
+          <h1 className='max-w-[496px] font-bold text-[40px]'>
+            Learn new skills online with ease
+          </h1>
+          <p className='max-w-[902px] text-[30px]'>
+            Discover a wide range of courses covering a variety of subjects, taught by expert instructors.
+          </p>
 
-      <button className='bg-black text-white w-[180px] font-medium text-[30px] h-[58px] flex items-center justify-center rounded-[5px]'>Start learn</button>
-      <button className='border-[1px] border-black w-[250px] font-medium text-[30px] h-[58px] flex items-center justify-center rounded-[5px]'>Explore Courses</button>
+          <div className='flex justify-between gap-[16px]'>
+            <button className='bg-black text-white w-[178px] font-medium text-[14px] h-[48px] flex items-center justify-center rounded-[5px]'>
+              Start Learning Now
+            </button>
+            <button className='border-[1px] border-black w-[178px] font-medium text-[14px] h-[48px] flex items-center justify-center rounded-[5px]'>
+              Explore Courses
+            </button>
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className='flex justify-center items-center w-1/2'>
+          <Image src={Girl} alt='teacher' width={720} height={900} />
+        </div>
       </div>
-      
-      <div className='flex justify-center items-center w-1/2'>
-        <Image src={Girl} alt='teacher' width={720} height={900}/>
+
+      {/* Banner Section */}
+      <div className='w-full'>
+        <Image src={Banner} alt='banner' layout='responsive' width={1280} height={228} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
